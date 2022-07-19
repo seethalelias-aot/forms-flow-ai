45f8050279b6e289c38f5ede59c714ed92c017a7
seethalelias-aot <seethal.elias@aot-technologies.com>
2022-07-19 11:46:58 +0530

    getbyidcase

--- forms-flow-api/tests/unit/api/test_draft.py
+++ forms-flow-api/tests/unit/api/test_draft.py
@@ -26,9 +26,30 @@ def test_draft_create_method(app, client, session, jwt):
     )
     assert rv.status_code == 201
     response = client.post("/draft", headers=headers, json=get_draft_create_payload(form_id))
-    print(response.json)
     assert response.status_code == 201
     assert response.json.get("id") is not None
+
+def test_draft_detail_view(app, client, session, jwt):
+    """Testing form process mapper details endpoint."""
+    token = get_token(jwt)
+    headers = {"Authorization": f"Bearer {token}", "content-type": "application/json"}
+    rv = client.post("/form", headers=headers, json=get_form_request_payload())
+    assert rv.status_code == 201
+
+    form_id = rv.json.get("formId")
+
+    rv = client.post(
+        "/application/create",
+        headers=headers,
+        json=get_application_create_payload(form_id),
+    )
+    assert rv.status_code == 201
+    response = client.post("/draft", headers=headers, json=get_draft_create_payload(form_id))
+    assert response.status_code == 201
+    draft_id = response.json.get("id")
+    rv = client.get(f"/draft/{draft_id}", headers=headers)
+    assert rv.status_code == 200
+    assert rv.json.get("id") == draft_id
 
 # def test_draft_update_details_api(app, client, session, jwt):
 #     """Tests the draft update endpoint with valid payload."""
